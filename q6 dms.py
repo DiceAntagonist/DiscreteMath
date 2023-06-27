c32e473dd8db0a05d228f84358e1036cea3703fc
DiceAntagonist <126333279+DiceAntagonist@users.noreply.github.com>
2023-06-27 15:05:16 +0530

    Update q6 dms.py

--- q6 dms.py
+++ q6 dms.py
@@ -1,8 +1,8 @@
 def is_complete_graph(adj_matrix):
-    # Get the number of vertices in the graph
+    
     num_vertices = len(adj_matrix)
 
-    # Check if each pair of distinct vertices is connected
+    
     for i in range(num_vertices):
         for j in range(i + 1, num_vertices):
             if adj_matrix[i][j] != 1:
@@ -10,7 +10,7 @@ def is_complete_graph(adj_matrix):
 
     return True
 
-# Example usage
+
 adjacency_matrix = [
     [0, 1, 1, 1],
     [1, 0, 1, 1],
